aa0efae741d407880bd05733f008c5d5b225857b
JanusLand <januslandsberg@gmail.com>
2025-09-05 23:59:17 +0200

    Update streamlit_app.py
    Corrected syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
+st.stop()
 
 ingredients_list = st.multiselect(
     label='Choose up to 5 ingredients:'
